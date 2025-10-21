529fa0993ffba2c4de47d3490d1879ed7e6b120a
Jesterios <110119801+Jesterios@users.noreply.github.com>
2025-10-21 22:18:41 +0300

    Update AlarmReceiver.kt

--- app /src/main/java/com/alarm/AlarmReceiver.kt
+++ app /src/main/java/com/alarm/AlarmReceiver.kt
@@ -3,18 +3,10 @@ package com.alarm
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.widget.Toast
 
 class AlarmReceiver : BroadcastReceiver() {
     
     override fun onReceive(context: Context, intent: Intent) {
-        // Здесь будет голос Иллидана
-        Toast.makeText(
-            context, 
-            "Иллидан: 'Проснись, смертный! Ты готов к жертве?'", 
-            Toast.LENGTH_LONG
-        ).show()
-        
         // Запускаем сервис для голосового будильника
         val serviceIntent = Intent(context, AlarmService::class.java)
         context.startService(serviceIntent)
